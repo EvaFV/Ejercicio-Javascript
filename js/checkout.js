dba9868252326dbace54ddf26108749279a368f3
Eva <eva.fer977@gmail.com>
2021-11-23 14:59:13 +0100

    falta terminar validaciones

--- js/checkout.js
+++ js/checkout.js
@@ -1,15 +1,70 @@
 var password = document.querySelector(".password");
 var phone = document.querySelector('.phone');
 var name = document.querySelector('.name');
-
+var email = document.querySelector(".mail");
 // Get the error elements
 var errorPassword = document.getElementById("errorPassword");
 var errorName = document.getElementById('errorName');  
-var errorPhone = document.getElementById('errorPhone');  
+var errorPhone = document.getElementById('errorPhone');
+var errorMail = document.getElementById('errorMail');
+
 
 // Exercise 8
 function validate() {
     // Validate fields entered by the user: name, phone, password, and email
 
+    errorName.style.display = 'none';
+    errorPhone.style.display = 'none';
+    errorPassword.style.display = 'none';
+    errorMail.style.display = 'none';
+    name1.classList.remove('is-invalid');
+    last.classList.remove('is-invalid');
+    email.classList.remove('is-invalid');
+    password.classList.remove('is-invalid');
+    address.classList.remove('is-invalid');
+    phone.classList.remove('is-invalid');
+
     
+    const regEx = /^[A-Za-z]+$/;
+    const numbers = /^[0-9]+$/;
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{4,8}$/;
+    const mailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    
+    // Validación nombre
+    if (name1.value == '' || name1.value.length < 3 || !name1.value.match(regEx)) {
+        name1.classList.add('is-invalid');
+        errorName.style.display = 'block';
+    }
+    // Validación last name
+    if (last.value == '' || last.value.length < 3 || !last.value.match(regEx)) {
+        last.classList.add('is-invalid');
+        errorName.style.display = 'block';
+    }
+    // Validación Email
+    if (email.value == '' || email.value.length < 3 || !email.value.match(mailRegex)) {
+        email.classList.add('is-invalid');
+        errorMail.style.display = 'block';
+    }
+    // Validación Password 
+    if (password.value == '' || password.value.length < 3 || !password.value.match(passwordRegex)) {
+        password.classList.add('is-invalid');
+        errorPassword.style.display = 'block';
+    }
+    // Validación Address   
+    if (address.value == '' || address.value.length < 3) {
+        address.classList.add('is-invalid');
+    }
+    // Validación Phone
+    if (phone.value == '' || phone.value.length < 3 || !phone.value.match(numbers)) {
+        phone.classList.add('is-invalid');
+        errorPhone.style.display = 'block';
+    } else {
+        return true;
+    }
+    alert('Todos los campos son requeridos y han de tener un mínimo de 3 caracteres');
 }
+
+
+
+
--- js/shop.js
+++ js/shop.js
@@ -86,18 +86,33 @@ var total = 0;
 function buy(id) {
 
     for (let i = 0; i < products.length; i++) {
-        if (id == i + 1) {
-            cartList.push(products[i])
-        }
-    }       console.log(cartList)
-}
+        if (products.indexOf(products[i]) + 1 === id) {
+            console.log(id);
+            addItem(i);
+    }      
+    }
     
+    function addItem(i) {
+        const item = { name: products[i].name, price: products[i].price, type: products[i].type, count: 1 }
+    
+    console.log(item);
+    for (let x in  cartList) {
+        if (cartList[x].name === item.name) {
+            cartList[x].count++;
+        }
+    }
+    cartList.push(item);
+    console.log(cartList);
+    }
+    calculateSubtotals();
+}
 
 // Ejercicio - 2
 
 function cleanCart() {
-    var cartList = [];
-    var cart = [];
+    
+    cartList.length = 0;
+    console.log(cartList);
 
 }
 
@@ -106,27 +121,39 @@ function calculateSubtotals() {
     // 1. Cree un bucle for en la matriz "cartList"
     // 2. Implementar dentro del loop un caso if ... else o switch ... para sumar las cantidades de cada tipo de producto, obteniendo los subtotales: subtotalGrocery, subtotalBeauty y subtotalClothes 
     
+    subtotal.grocery.value = 0;
+    subtotal.beauty.value = 0;
+    subtotal.clothes.value = 0;
+           
+    // for (let i = 0; i < cartList.length; i++) {
+    //     if (cartList[i].type == "grocery") {
+    //         subtotal.grocery.value = subtotal.grocery.value + cartList[i].price;
+    //     } else if (cartList[i].type == "beauty") {
+    //         subtotal.beauty.value = subtotal.beauty.value + cartList[i].price;
+    //     } else if (cartList[i].type == "clothes") {
+    //         subtotal.clothes.value = subtotal.clothes.value + cartList[i].price;
+    //     }   else {
+    //         console.log("Tipo no encontrado");
+    //     }
+    // }
+   
     for (let i = 0; i < cartList.length; i++){
-        if (cartList.length > 0) {
-            switch (cartList[i].type) {
-                case 'grocery':
-                    subtotal.grocery.value += cartList[i].price
-                    break;
-                case 'beauty':
-                    subtotal.beauty.value += cartList[i].price
-                    break;
-                case 'clothes':
-                    subtotal.clothes.value += cartList[i].price
-                    break;
-                default:
-                    console.log('El carrito está Vacío')
-            }
-            
-        } else  {
-            console.log('El carrito está vacío ')
+        var price = cartList[i].price;
+        if (cartList[i].type === 'grocery') {
+            subtotalGrocery += price;
+        } else if (cartList[i].type === 'beauty') {
+            subtotalBeauty += price;
+        } else {
+            subtotalClothes += price;
         }
     }
-            console.log('El subtotal de grocery es: ' + subtotal.grocery.value + '\n' + 'El subtotal de beauty es: ' + subtotal.beauty.value + '\n' + 'El subtotal de clothes es: ' + subtotal.clothes.value)
+    subtotal.grocery.value = subtotalGrocery;
+    subtotal.beauty.value = subtotalBeauty;
+    subtotal.clothes.value = subtotalClothes;
+    console.log(subtotal);
+
+    calculateTotal();
+    
 }
 
 
@@ -134,10 +161,11 @@ function calculateSubtotals() {
 function calculateTotal() {
     // 1. Calcule el precio total del carrito usando la matriz "cartList"
     let total = 0;
-    for (let x in subtotal) {
-        total += subtotal[x].value;
+    let totalValue = [subtotal.grocery.value, subtotal.beauty.value, subtotal.clothes.value];
+    for (let x in totalValue) {
+        total += totalValue[x];
     }
-    return total
+    console.log('Total sin descuentos  ' + total);
 }
 
 // Ejercicio - 5
@@ -145,49 +173,74 @@ function generateCart() {
     /**  Usando la matriz "lista de carritos" que contiene todos los artículos en el carrito de compras,
      generar la matriz "carrito" que no contiene elementos repetidos, en su lugar, cada elemento de esta matriz "carrito" muestra la cantidad de producto.*/
     
-    for (let i = 0; i < cartList.length; i++) {
-        if (!cart.includes(cartList[i])) {
-            cart.push(cartList[i])
-            cart[cart.length - 1].quantity = 1
-        }
-        else {
-            for (let x = 0; x < cart.length; x++) {
-                if (cart[x].name == cartList[i].name) {
-                    cart[x].quantity += 1
-                    break;
-                }
+    // for (let i = 0; i < cartList.length; i++) {
+    //     if (!cart.includes(cartList[i])) {
+    //         cart.push(cartList[i])
+    //         cart[cart.length - 1].quantity = 1
+    //     }
+    //     else {
+    //         for (let x = 0; x < cart.length; x++) {
+    //             if (cart[x].name == cartList[i].name) {
+    //                 cart[x].quantity += 1
+    //                 break;
+    //             }
+    //         }
+    //     }
+    // }
+
+    cart.length = 0;
+    // 
+    cart.push(cartList[0]);
+    let currentName = cartList[0].name;
+    let max = cartList[0].count;
+    let maxItem = cartList[0];
+    for (let i = 1; i < cartList.length; i++) {
+        if (currentName != cartList[i].name) {
+            max = cartList[i].count;
+            currentName = cartList[i].name;
+            maxItem = cartList[i];
+            cart.push(maxItem);
+        } else if (currentName === cartList[i].name) {
+            if (cartList[i].count > max) {
+                currentName = cartList[i].name;
+                maxItem = cartList[i];
+                cart.push(maxItem);
             }
         }
     }
-    console.log(cart);
 
-    
     for (let i in cart) {
-        cart[i].subtotal = cart[i].price * cart[i].quantity;
+        cart[i].subtotal = cart[i].price * cart[i].count;
         cart[i].subtotalWithDiscount = cart[i].subtotal;
         
     }
     console.log(cart);
     applyPromotionsCart();
-    
-    // console.log('Total con el discount aplicado ' +);
-
-    
+    console.log('total con descuentos' + total);
 }
 
 // Ejercicio - 6
 function applyPromotionsCart() {
     // Aplicar promociones a dos artículos de la matriz "carrito"
-
-    for (let i = 0; i < cart.length; i++) {
-        if ((cart[i].name == 'cooking oil') && (cart[i].quantity > 3)) {
-            cart[i].subtotalWithDiscount = (((cart[i].price) - 0.5) * cart[i].quantity);
-        }
-        else if ((cart[i].name == 'Instant cupcake mixture') && (cart[i].quantity > 10)) {
-            cart[i].subtotalWithDiscount = (cart[i].price * cart[i].quantity) * (2 / 3);
+    
+    for (let i in cart) {
+        subtotal.grocery.discount = 0;
+        if (cart[i].name === 'cooking oil' && cart[i].count > 3) {
+            const oilDiscount = cart[i].count * 0.5;
+            cart[i].subtotalWithDiscount -= oilDiscount;
+            subtotal.grocery.discount = oilDiscount;
+            total -= oilDiscount;
+            console.log('Cooking oil subtotal discounted ' + cart[i].subtotalWithDiscount);
+        } else if (cart[i].name === 'Instant cupcake mixture' && cart[i].count > 10) {
+            const mixDiscount = (cart[i].price / 3) * cart[i].count;
+            cart[i].subtotalWithDiscount -= mixDiscount;
+            subtotal.grocery.discount += mixDiscount;
+            total -= mixDiscount
+            console.log('Cupcake mixture subtotal discounted ' + cart[i].subtotalWithDiscount);
         }
     }
 }
+
 
  
 
@@ -198,25 +251,54 @@ function addToCart(id) {
      // 2. Agregue el producto encontrado a la matriz del carrito o actualice su cantidad en caso de que haya sido agregado previamente. 
     
     for (let i = 0; i < products.length; i++){
-        if (id == i + 1) {
-            if (!cart.includes(products[i])) {
-                cart.push(products[i])
-                cart[cart.length - 1].quantity = 1
+        
+            if (products.indexOf(products[i]) + 1 === id) {
+                console.log(id);
+                addItem(i);
             }
-            else {
-                for (let x = 0; x < cart.length; x++){
-                    if (cart[x].name == products[i].name) {
-                        cart[x].quantity += 1
-                        break;
-                    }
+    }
+    function addItem(i) {
+        const item = {
+            name: products[i].name, price: products[i].price, type: products[i].type, count: 1,}
+        console.log(item);
+
+        if (cart.length === 0) {
+            cart.push(item);
+            console.log(cart);
+        } else {
+            for (let i = 0; i < cart.length; i++) {
+                if (item.name === cart[i].name) {
+                    cart[i].count++
+                    result = true;
+                    return result;
+                        
+                }
+            }
+            
+            for (let i = 0; i < cart.length; i++) {
+                if (item.name !== cart[i].name) {
+                    cart.push(item);
+                    return;
                 }
             }
         }
     }
-
+    
+    //Añado propiedades a cart
+    for (let i in cart) {
+        cart[i].subtotal = cart[i].price * cart[i].count;
+        cart[i].subtotalWithDiscount = cart[i].subtotal;
+    }
     console.log(cart);
-    
-    
+
+    //escribo el total cuando se adjunta el producto
+    total = 0;
+    for (let i in cart) {
+        total += cart[i].subtotal;
+    }
+    console.log(total);
+    applyPromotionsCart();
+    console.log('Total con el descuento:  ' + total);
     
 }
 
@@ -224,16 +306,59 @@ function addToCart(id) {
 function removeFromCart(id) {
     // 1. Recorra los productos de la matriz para que el artículo se agregue al carrito.
     // 2. Agregue el producto encontrado a la matriz cartList
-    if (cart.length > 0) {
-            
+    for (let i in cart) {
+        if (cart[i].name === event.target.dataset.name) {
+            if (cart[i].count === 1) {
+                cart.splice(i, 1);
+            } else if (cart[i].count > 1) {
+                cart[i].count--;
+                cart[i].subtotal -= cart[i].price;
+            }
         }
+    }
+    // total cuando se borra un producto
+    total = 0;
+    for (let i in cart) {
+        total += cart[i].subtotal;
+    }
+    console.log(total);
+    // Para aplicar el descuento si es necesario cuando se borra un producto
+    applyPromotionsCart();
+    console.log('Total con el descuento:  ' + total);
+    printCart();
+
 
 }
 // Ejercicio - 10
 function printCart() {
     // 1. Llenar el carrito de la compra modal manipulando el carrito de la compra dom
-
+    
+      const cartItems = document.getElementsByClassName('cart-items')[0];
+    cartItems.replaceChildren();
+    for (let x in cart) {
+        const cartRow = document.createElement('li');
+        cartRow.classList.add('cart-row');
+        const cartRowContent = `
+                <div class="item-column item-name col-4">${cart[x].name}</div>
+                <div class="item-colun item-price col-2">${cart[x].price}€</div>
+                <div class="item-column item-quantity col-3">
+                    <div class="item-column item-quantity">${cart[x].count}</div>
+                    <button type="button" onclick="removeFromCart(event)" data-name="${cart[x].name}" class="item-column item-quantity minus-item bg-secondary">-</div>
+                </div>    
+                <div class="item-column item-subtotal col-3">${cart[x].subtotal}€</div>`
+        cartRow.innerHTML = cartRowContent;
+        cartItems.append(cartRow);
+    }
+    
+    const discountCart = document.getElementById('discount');
+    discountCart.innerHTML = Math.round(subtotal.grocery.discount * 10) / 10 + '€';
+    const totalCart = document.getElementById('total');
+    totalCart.innerHTML = Math.round(total * 10) / 10 + '€';
+    
+ 
+    
 }
+
 
 
 
